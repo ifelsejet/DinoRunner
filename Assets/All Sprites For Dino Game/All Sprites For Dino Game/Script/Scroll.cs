b13cb7df07443ad35e6052d4a1eae1b729108423
Jatnael Montes <ifelsejet@gmail.com>
2019-09-16 21:11:48 -0700

    Added Workshop Day 2 (Day 1 for me) Code

---
+++ Assets/All Sprites For Dino Game/All Sprites For Dino Game/Script/Scroll.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Material material; //Unity needs material to be attached to
+    public float xVel = 0.1f; //Setting our start point 0.1 units away
+    Vector2 offset;
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset = new Vector2(xVel, 0);
+        material.mainTextureOffset += offset * Time.deltaTime; //Updating textures with offset update every 0.1 seconds
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DinoRunner
  Unity based project based off the offline Google Chrome game. Shout out to the UCM Game Development for this workshop series!
+
+ ML or AI to solve the game on it's own is something to do in the future.
